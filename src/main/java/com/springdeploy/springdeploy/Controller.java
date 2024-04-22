50a2cabae49b8d863c1a74453096200176a0215a
mrsluggan <ericflyger@gmail.com>
2024-04-22 11:59:27 +0200

    first

---
+++ src/main/java/com/springdeploy/springdeploy/Controller.java
@@ -0,0 +1,17 @@
+package com.springdeploy.springdeploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    
+}
